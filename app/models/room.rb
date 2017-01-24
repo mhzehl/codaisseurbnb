98d84515456e8ca3c08a36286984d8f1b8a6212f
Melvin Zehl <mhzehl@gmail.com>
2017-01-24 13:49:33 +0100

    Create API controllers and modify routes

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_and_belongs_to_many :themes
   has_many :photos
   has_many :bookings, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :bookings, only: [:create]
   end
-  
+
   resources :profiles, only: [:new, :edit, :create, :update]
   resources :photos
+
+  namespace :api do
+    resources :rooms do
+      resources :bookings, only: [:create, :update, :destroy]
+    end
+  end
 end
